22f32422887e000cd6cf87f6054ab3c36031080e
SHAKHAWAT <shihabchtg@gmail.com>
2022-10-12 17:04:36 +0600

    db failed on uri

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const dotenv = require("dotenv").config();
 // database connection
 mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
     console.log(`Database connection is successful`);
-})
-
+},
+    err => { console.log(`Failed to connect`, err); }
+)
 
 const app = require("./app");
-
-
-
 
 // server
 const port = process.env.PORT || 8080;
